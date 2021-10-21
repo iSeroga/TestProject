cb851179ff8c4c449f9bb4a40d9f7af63b57e2af
iSeroga <58987961+iSeroga@users.noreply.github.com>
2021-10-21 21:42:07 +0300

    fixed

--- first week/main.js
+++ first week/main.js
@@ -24,3 +24,34 @@ site.addEventListener('click', e=> {
     }
 });
 
+const askW = document.querySelector('.ask');
+let agressor = document.querySelector('.openB');
+agressor.onclick = function () {
+    askW.classList.toggle('open-window');
+}
+
+
+let Ybut = document.querySelector('.YesB');
+let ClosedNumb = document.querySelector('.closed');
+let Nbut = document.querySelector('.NoB');
+let NumbAgr = agressor.textContent;
+let NumrClos = ClosedNumb.textContent;
+Ybut.onclick = function() {
+    if (NumbAgr > 0){
+        NumbAgr = NumbAgr -1;
+
+        NumrClos = +NumrClos + 1;
+
+        askW.classList.toggle('open-window');
+    }
+    else {
+        askW.classList.toggle('open-window');
+    }
+
+    agressor.innerHTML= NumbAgr;
+    ClosedNumb.innerHTML= NumrClos;
+}
+Nbut.onclick = function () {
+
+    askW.classList.toggle('open-window');
+}
